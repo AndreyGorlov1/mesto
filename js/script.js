ac1e809f8f722f2b3f0fd5c106e78f9b7f708b3a
Андрей <andrey.g0rlov@yandex.ru>
2023-03-11 19:13:49 +0400

    Все по новой

---
+++ js/script.js
@@ -0,0 +1,168 @@
+import { Card } from './card.js';
+import { FormValidator } from './formValidator.js';
+
+
+const mainName = document.querySelector('.profile__main-name');
+const extra = document.querySelector('.profile__activity');
+const editProfileForm = document.querySelector('#editProfile');
+const nameInput = document.querySelector('#input__name');
+const extraInput = document.querySelector('#input__extra');
+const editProfilePopup = document.querySelector('#editProfilePopup');
+const editProfile = document.querySelector('.profile__edit-button');
+
+const addCard = document.querySelector('.profile__add-button');
+const cardSection = document.querySelector('.elements');
+const addMestoForm = document.querySelector('#addMesto');
+const addMestoPopup = document.querySelector('#addMestoPopup');
+const addCardName = document.querySelector('#addCardName');
+const addCardExtra = document.querySelector('#addCardExtra');
+const closeButtons = document.querySelectorAll('.popup__close');
+
+const popupsList = Array.from(document.querySelectorAll('.popup'));
+
+const profileNameField = document.querySelector('.form__field_profile-name');
+const profileExtraField = document.querySelector('.form__field_profile-extra');
+const cardAddNameField = document.querySelector('.form__field_add-name');
+const cardAddExtraField = document.querySelector('.form__field_add-extra');
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+initialCards.forEach(function(item) {
+  createCard(item.name, item.link);
+});
+
+function createCard(name, link) {
+  const card = new Card(name, link);
+
+  const cardElement = card.generateCard();
+
+  cardSection.prepend(cardElement);
+}
+
+popupsList.forEach(function(item) {
+  item.addEventListener('click', function(evt) {
+    closePopup(evt.target);
+  });
+});
+
+closeButtons.forEach(function(button) {
+  const popup = button.closest('.popup');
+
+  button.addEventListener('click', function() {
+    closePopup(popup);
+  });
+});
+
+const closeOnKey = function(evt) {
+  if (evt.key === 'Escape') {
+  const openedPopup = document.querySelector('.popup_opened');
+    closePopup(openedPopup);
+  };
+}
+
+function openPopup(item) {
+  item.classList.add('popup_opened');
+  document.addEventListener('keydown', closeOnKey);
+};
+function closePopup(item) {
+  item.classList.remove('popup_opened');
+  document.removeEventListener('keydown', closeOnKey);
+};
+
+function handleEditFormSubmit(evt) {
+  evt.preventDefault();
+
+  mainName.textContent = nameInput.value;
+  extra.textContent = extraInput.value;
+
+  closePopup(editProfilePopup);
+};
+
+function handleAddFormSubmit(evt) {
+  evt.preventDefault();
+
+  createCard(addCardName.value, addCardExtra.value);
+
+  evt.target.reset();
+
+  closePopup(addMestoPopup);
+};
+
+editProfileForm.addEventListener('submit', handleEditFormSubmit);
+addMestoForm.addEventListener('submit', handleAddFormSubmit);
+
+editProfile.addEventListener('click', function() {
+  openPopup(editProfilePopup);
+
+  nameInput.value = mainName.textContent;
+  extraInput.value = extra.textContent;
+  
+  const nameInputValidation = new FormValidator({
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit',
+    inputErrorClass: 'form__input_invalid',
+    spanClass: '.form__error-span',
+    spanErrorClass: 'form__error-span_enabled',
+  }, profileNameField);
+
+  nameInputValidation.enableValidation();
+
+  const extraInputValidation = new FormValidator({
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit',
+    inputErrorClass: 'form__input_invalid',
+    spanClass: '.form__error-span',
+    spanErrorClass: 'form__error-span_enabled',
+  }, profileExtraField);
+
+  extraInputValidation.enableValidation();
+});
+
+addCard.addEventListener('click', function() {
+  openPopup(addMestoPopup);
+
+  const nameInputValidation = new FormValidator({
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit',
+    inputErrorClass: 'form__input_invalid',
+    spanClass: '.form__error-span',
+    spanErrorClass: 'form__error-span_enabled',
+  }, cardAddNameField);
+
+  nameInputValidation.enableValidation();
+
+  const extraInputValidation = new FormValidator({
+    inputSelector: '.form__input',
+    submitButtonSelector: '.form__submit',
+    inputErrorClass: 'form__input_invalid',
+    spanClass: '.form__error-span',
+    spanErrorClass: 'form__error-span_enabled',
+  }, cardAddExtraField);
+
+  extraInputValidation.enableValidation();
+});
---
+++ js/validate.js
@@ -0,0 +1,68 @@
+function enableValidation() {
+  const formList = Array.from(document.querySelectorAll('.form'));
+  formList.forEach(function(formElement) {
+    setEventLiestener(formElement);
+    formElement.addEventListener('submit', function (evt) {
+      evt.preventDefault();
+      enableValidation(); //Думаю, так хорошо работает.
+    });
+  });
+};
+
+function setEventLiestener(formElement) {
+  const inputList = Array.from(formElement.querySelectorAll('.form__input'));
+
+  const buttonElement = formElement.querySelector('.form__submit');
+  toggleSubmitButtonState(buttonElement, inputList);
+
+  inputList.forEach(function(inputElement) {
+    // console.log(inputElement.validity.valid);
+    // Я непонимаю почему, но поля ввода во всех формах изначально false, хотя в редактировании профиля поля валидны...
+    
+
+    const errorSpan = inputElement.closest('.form__field').querySelector('.form__error-span');
+    
+    inputElement.addEventListener('input', function() {
+      checkInputValidity(inputElement,errorSpan);
+      toggleSubmitButtonState(buttonElement, inputList);
+    });
+  });
+};
+
+function toggleSubmitButtonState(button, inputList) {
+  if (hasInvalidInput(inputList)) {
+    button.setAttribute('disabled', '');
+  } else {
+    button.removeAttribute('disabled');
+  };
+};
+
+function checkInputValidity (formInput, errorSpan) {
+  if (!formInput.validity.valid) {
+    showErrorMessage(errorSpan, formInput.validationMessage);
+    formInput.classList.add('form__input_invalid');
+  } else if (formInput.validity.valid) {
+    hideErrorMessage(errorSpan);
+    formInput.classList.remove('form__input_invalid');
+  };
+};
+
+function showErrorMessage (element, errorMessage) {
+  element.classList.add('form__error-span_enabled');
+
+  element.textContent = errorMessage;
+};
+
+function hideErrorMessage (element) {
+  element.classList.remove('form__error-span_enabled');
+
+  element.textContent = '';
+};
+
+enableValidation();
+
+function hasInvalidInput(inputArray) {
+  return inputArray.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+};
